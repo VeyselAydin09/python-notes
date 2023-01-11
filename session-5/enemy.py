e7292ce6889f95bf315bf8a4c773b7e7f15576fe
Veysel Aydin <weycell.aydin09@gmail.com>
2023-01-11 19:04:54 +0300

    enemy finish

--- session-5/enemy.py
+++ session-5/enemy.py
@@ -25,3 +25,39 @@ class Enemy(object):
 
     def __str__(self):
         return f"Name: {self._name}, Lives: {self._lives}, Hit points: {self._hit_points}"
+
+        class Troll(Enemy):
+
+    def __init__(self, name):
+        # super(Troll, self).__init__(name=name, lives=1, hit_points=23)
+        super().__init__(name=name, lives=1, hit_points=23)
+
+    def grunt(self):
+        print(f"Me {self._name}. {self._name} stomp you")
+
+
+class Vampyre(Enemy):
+
+    def __init__(self, name):
+        super().__init__(name=name, lives=3, hit_points=12)
+
+    def dodges(self):
+        if random.randint(1, 3) == 3:
+            print(f"***** {self._name} dodges *****")
+            return True
+        else:
+            return False
+
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage=damage)
+
+
+class VampyreKing(Vampyre):
+
+    def __init__(self, name):
+        super().__init__(name)
+        self._hit_points = 140
+
+    def take_damage(self, damage):
+        super().take_damage(damage // 4)
