a0398fe25f5e2f76308baae057a8f0e269b4cbfa
Veysel Aydin <weycell.aydin09@gmail.com>
2023-01-13 13:00:21 +0300

    OOP

---
+++ session-5/OOP1.py
@@ -0,0 +1,23 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+print("--------------------------------------")
+
+#! Topics to be Covered:
+#* Everything in Python is class
+#* Defining class
+#* Defining class attributes
+#* Difference between class attributes and instance attributes
+#* SELF keyword
+#* Defining methods
+#* Class Methods vs. Static Methods and Instance Methods
+#* Special methods (init, str)
+#* 4 pillars of OOP:
+    #? Encapsulation
+    #? Abstraction
+    #? Inheritance
+        #? Multiple inheritance
+    #? Polymorphism
+        #? Overriding methods
+#* extra subjects
+#* Inner class
+
